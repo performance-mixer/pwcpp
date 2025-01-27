33fb01ecd3c334fb8750c0fdad6487436e8df8ef
Frank Krick <frank.krick@gmail.com>
2025-01-26 20:41:20 -0500

    Continued documentation

--- include/pwcpp/filter/filter_port.h
+++ include/pwcpp/filter/filter_port.h
@@ -10,16 +10,35 @@
 
 namespace pwcpp::filter {
 
+/*! \brief Dequeue a buffer from a pipewire port.
+ *
+ * Normally supplied in the default constructor and only overridden for unit
+ * tests.
+ */
 using pipewire_buffer_dequeue =
     std::function<pw_buffer *(struct pwcpp::filter::port *)>;
 
+/*! \brief C++ wrapper for a pipewire port.
+ *
+ * This class wraps a pipewire port and provides a C++ interface for
+ * interacting with it.
+ */
 class FilterPort {
 public:
+  /*! \brief Construct a filter port for testing.
+   */
   FilterPort(pipewire_buffer_dequeue buffer_dequeue,
              pipewire_buffer_enqueue buffer_enqueue)
       : port(nullptr), buffer_dequeue(buffer_dequeue),
         buffer_enqueue(buffer_enqueue) {}
 
+  /*! \brief Construct a filter port.
+   *
+   * The construtor is normally not used directly. Pwcpp creates the port when
+   * the filter app builder builds the filter.
+   *
+   * \param port The pipewire port to wrap.
+   */
   FilterPort(struct pwcpp::filter::port *port)
       : port(port), buffer_dequeue([](struct pwcpp::filter::port *port) {
           return pw_filter_dequeue_buffer(port);
@@ -28,6 +47,10 @@ public:
           pw_filter_queue_buffer(port, buffer);
         }) {}
 
+  /*! \brief Get the buffer from the port.
+   *
+   * \return A buffer if one is available, otherwise an empty optional.
+   */
   std::optional<pwcpp::Buffer> get_buffer() {
     auto buffer = buffer_dequeue(port);
     if (buffer == nullptr)
--- include/pwcpp/filter/parameter.h
+++ include/pwcpp/filter/parameter.h
@@ -6,19 +6,42 @@
 
 namespace pwcpp::filter {
 
+/*! \brief The variant stores the parameter value.
+ */
 using variant_type =
     std::variant<int, float, double, std::string, std::nullopt_t>;
 
+/*! \brief A pipewire parameter.
+ */
 class Parameter {
 public:
+  /*! \brief Construct a parameter.
+   *
+   * \param key The parameter key.
+   * \param id The parameter id.
+   * \param value The parameter value.
+   */
   Parameter(std::string key, size_t id, variant_type value)
       : key(key), id(id), value(value) {}
 
-  std::string key;
-  size_t id;
-  variant_type value;
-
+  /*! \brief Get the parameter value.
+   *
+   * \return The parameter value.
+   */
   template <typename T> const T &get() const { return std::get<T>(value); }
+
+  /*! \brief The parameter key. */
+  std::string key;
+
+  /*! \brief The parameter id.
+   *
+   * The parameter id is freely choosen when adding the parameter to the filter
+   * builder and can be used to identify parameters in code.
+   */
+  size_t id;
+
+  /*! \brief The parameter value. */
+  variant_type value;
 };
 
 } // namespace pwcpp::filter
